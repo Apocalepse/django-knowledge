783063526644c4c875472d644f19e101af6ec646
Apocalepse <apocalepse@gmail.com>
2017-01-07 10:19:46 +0500

    django 1.9+ fixes

--- knowledge/urls.py
+++ knowledge/urls.py
@@ -1,22 +1,21 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+from knowledge import views
 
-urlpatterns = patterns('knowledge.views',
-    url(r'^$', 'knowledge_index', name='knowledge_index'),
 
-    url(r'^questions/$', 'knowledge_list', name='knowledge_list'),
+urlpatterns = [
+    url(r'^$', views.knowledge_index, name='knowledge_index'),
 
-    url(r'^questions/(?P<question_id>\d+)/$',
-        'knowledge_thread', name='knowledge_thread_no_slug'),
+    url(r'^questions/$', views.knowledge_list, name='knowledge_list'),
 
-    url(r'^questions/(?P<category_slug>[a-z0-9-_]+)/$', 'knowledge_list',
-        name='knowledge_list_category'),
+    url(r'^questions/(?P<question_id>\d+)/$', views.knowledge_thread, name='knowledge_thread_no_slug'),
 
-    url(r'^questions/(?P<question_id>\d+)/(?P<slug>[a-z0-9-_]+)/$',
-        'knowledge_thread', name='knowledge_thread'),
+    url(r'^questions/(?P<category_slug>[a-z0-9-_]+)/$', views.knowledge_list, name='knowledge_list_category'),
+
+    url(r'^questions/(?P<question_id>\d+)/(?P<slug>[a-z0-9-_]+)/$', views.knowledge_thread,
+        name='knowledge_thread'),
 
     url(r'^moderate/(?P<model>[a-z]+)/'
-        r'(?P<lookup_id>\d+)/(?P<mod>[a-zA-Z0-9_]+)/$',
-        'knowledge_moderate', name='knowledge_moderate'),
+        r'(?P<lookup_id>\d+)/(?P<mod>[a-zA-Z0-9_]+)/$', views.knowledge_moderate, name='knowledge_moderate'),
 
-    url(r'^ask/$', 'knowledge_ask', name='knowledge_ask'),
-)
+    url(r'^ask/$', views.knowledge_ask, name='knowledge_ask'),
+]
